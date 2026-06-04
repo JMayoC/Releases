0fcfe3ad435f5705aab6a898a856ffe9c1d80f1b
JMayoC <73552216+JMayoC@users.noreply.github.com>
2026-06-04 10:08:05 -0600

    Actualizacion

--- Deploy/srv/AlloFrwl.bat
+++
@@ -1,2 +0,0 @@
-﻿netsh http add urlacl url=http://192.168.1.144:8080/ user=Everyone
-netsh advfirewall firewall add rule name="EsconHttp8080" dir=in action=allow protocol=TCP localport=8080
--- Deploy/srv/CerrarServidor.bat
+++
@@ -1,2 +0,0 @@
-﻿netsh http delete urlacl url=http://192.168.1.144:8080/
-netsh advfirewall firewall delete rule name="EsconHttp8080"
